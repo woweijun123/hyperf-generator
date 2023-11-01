d32acd46699ecf62545f6f84d704f38033dcdc3b
weichengjun <1054487195@qq.com>
2023-11-01 11:49:33 +0800

    data 添加 commonQuery

--- src/Command/tpl/data.php
+++ src/Command/tpl/data.php
@@ -4,6 +4,7 @@ namespace App\Data\<?= $nameSpace ?>;
 
 use App\Data\Base\DataTrait;
 use App\Model\<?= $nameSpace ?>\<?= $modelName ?>Model;
+use Hyperf\Database\Model\Builder;
 use Hyperf\Di\Annotation\Inject;
 
 class <?= $modelName ?>Data
@@ -12,4 +13,19 @@ class <?= $modelName ?>Data
 
     #[Inject]
     public <?= $modelName ?>Model $model;
+
+    /**
+    * @param array $condition
+    * @return Builder
+    */
+    public function commonQuery(array $condition): Builder
+    {
+        $builder = $this->model->newQuery();
+        $pk      = $this->model->getKeyName();
+        if (!empty($condition[$pk])) {
+            $builder->where($pk, $condition[$pk]);
+        }
+
+        return $builder;
+    }
 }
